1ab5e4f5f2785ef6577d22caa91a8dfb4e89afdf
Android <umairasad001@gmail.com>
2022-09-22 16:29:43 +0500

    This is third commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("This is contained in my second commit");
+
+        Today is a good day
     }
 }
